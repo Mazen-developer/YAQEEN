bedc85bef88ef52881e72c1367a0e73203b99341
Mazen <mazenzayed624@gmail.com>
2026-08-19 14:50:55 +0300

    Update project

--- components/Header.tsx
+++ components/Header.tsx
@@ -30,7 +30,7 @@ export default function Header() {
             rel="noopener noreferrer"
             className="flex items-center gap-2 rounded-full border-[1.5px] border-white bg-transparent px-4 py-2 text-sm font-black text-white transition hover:bg-white/10"
           >
-            ✨ تصميم خاص
+            + تصميم خاص
           </a>
 
           <Link
